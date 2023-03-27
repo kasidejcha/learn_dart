f5e5729a5452de1462b5ff499d63140037b712c1
Kasidej Cha-umpong <kasidejcha.bas@gmail.com>
2023-03-27 10:11:26 +0700

    switch_case

--- if_else_class.dart
+++ if_else_class.dart
@@ -21,6 +21,20 @@ void main() {
   check_number = n?.number ?? 0;
   print(check_number);
 
+  int x = 100;
+  var result = x%2 == 0 ? 'Even' :'Odd';
+  print(result);
+
+  int x1 = 51;
+  if (x1%2 == 0){
+    print('Even');
+  }
+  else if (x1%3 == 0){
+    print('Odd');
+  }
+  else{
+    print('Confused');
+  }
 }
 
 class class_number {
---
+++ switch_case.dart
@@ -0,0 +1,14 @@
+void main() {
+  int number = 1;
+  
+  switch(number) {
+    case 0:
+      print('number: zero');
+      break;
+    case 1:
+      print('number: one');
+      break;
+    default:
+      print('$number');
+  }
+}
